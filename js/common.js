ee14c4436cae689ff9da363a4c17c4260530dc68
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-09-23 13:25:28 +0300

    fixed bug

--- js/common.js
+++ js/common.js
@@ -4,22 +4,25 @@ $(document).ready(function () {
         $('select').styler();
     });
     // left fixed nav
-    $(function () {
-        $(document).scroll(function () {
-            if ($(this).scrollTop() >= $('#hello').offset().top - 50) {
-                $(".fixed-left-box ul li").removeClass('active');
-                $(".fixed-left-box ul li:nth-child(4)").addClass('active');
-            }
-            if ($(this).scrollTop() >= $('#rad').offset().top - 50) {
-                $(".fixed-left-box ul li").removeClass('active');
-                $(".fixed-left-box ul li:nth-child(3)").addClass('active');
-            }
-            if ($(this).scrollTop() >= $('#app').offset().top - 50) {
-                $(".fixed-left-box ul li").removeClass('active');
-                $(".fixed-left-box ul li:nth-child(2)").addClass('active');
-            }
+    if ($('.fixed-left-box').length > 0) {
+        $(function () {
+            $(document).scroll(function () {
+                if ($(this).scrollTop() >= $('#hello').offset().top - 50) {
+                    $(".fixed-left-box ul li").removeClass('active');
+                    $(".fixed-left-box ul li:nth-child(4)").addClass('active');
+                }
+                if ($(this).scrollTop() >= $('#rad').offset().top - 50) {
+                    $(".fixed-left-box ul li").removeClass('active');
+                    $(".fixed-left-box ul li:nth-child(3)").addClass('active');
+                }
+                if ($(this).scrollTop() >= $('#app').offset().top - 50) {
+                    $(".fixed-left-box ul li").removeClass('active');
+                    $(".fixed-left-box ul li:nth-child(2)").addClass('active');
+                }
+            });
         });
-    });
+    }
+
 
     // burger menu
     $('.phone-burger').click(function () {
@@ -28,4 +31,7 @@ $(document).ready(function () {
     $('.close-phone-menu').click(function () {
         $('.header__nav').hide();
     });
+
+    // scroll
+
 });
