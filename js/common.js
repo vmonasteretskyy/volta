d717e7c92733b00a4f94710ccee6f273d2e98f69
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-09-23 09:16:29 +0300

    home page

---
+++ js/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // styling select
+    $(function () {
+        $('select').styler();
+    });
+    // left fixed nav
+    $(function () {
+        $(document).scroll(function () {
+            if ($(this).scrollTop() >= $('#hello').offset().top - 50) {
+                $(".fixed-left-box ul li").removeClass('active');
+                $(".fixed-left-box ul li:nth-child(4)").addClass('active');
+            }
+            if ($(this).scrollTop() >= $('#rad').offset().top - 50) {
+                $(".fixed-left-box ul li").removeClass('active');
+                $(".fixed-left-box ul li:nth-child(3)").addClass('active');
+            }
+            if ($(this).scrollTop() >= $('#app').offset().top - 50) {
+                $(".fixed-left-box ul li").removeClass('active');
+                $(".fixed-left-box ul li:nth-child(2)").addClass('active');
+            }
+        });
+    });
+
+    // burger menu
+    $('.phone-burger').click(function () {
+        $('.header__nav').show();
+    });
+    $('.close-phone-menu').click(function () {
+        $('.header__nav').hide();
+    });
+});
